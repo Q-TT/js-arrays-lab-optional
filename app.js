40136208dd60c3815de939bdf47c8f5cc77545fe
Astrid Ge <qiutongg@umich.edu>
2024-07-07 20:51:55 -0700

    complete task 3 plus  challenge from Kyle

--- app.js
+++ app.js
@@ -33,126 +33,126 @@ foods.unshift("taco")
 console.log('Exercise 3 result:', foods);
 
 
-/*
-Exercise 4: Access an array element
+// /*
+// Exercise 4: Access an array element
 
-1) Retrieve the 'pizza' string from the array based on its position (index) in
-   the array.  
+// 1) Retrieve the 'pizza' string from the array based on its position (index) in
+//    the array.  
 
-2) Assign it to a variable called `favFood`.
+// 2) Assign it to a variable called `favFood`.
 
-Complete Exercise 4 in the space below:
-*/
+// Complete Exercise 4 in the space below:
+// */
 
 
 
 console.log('Exercise 4 result:', favFood);
 
 
-/*
-Exercise 5: Insert an element between two others
+// /*
+// Exercise 5: Insert an element between two others
 
-1) Insert the string 'tofu' between 'pizza' and 'cheeseburger' in the array.
+// 1) Insert the string 'tofu' between 'pizza' and 'cheeseburger' in the array.
 
-Complete Exercise 5 in the space below:
-*/
+// Complete Exercise 5 in the space below:
+// */
 
 
 
 console.log('Exercise 5 result:', foods);
 
 
-/*
-Exercise 6: Replace elements
+// /*
+// Exercise 6: Replace elements
 
-1) Replace 'pizza' in the `foods` array with 'sushi' and 'cupcake'.
+// 1) Replace 'pizza' in the `foods` array with 'sushi' and 'cupcake'.
 
-Complete Exercise 6 in the space below:
-*/
+// Complete Exercise 6 in the space below:
+// */
 
 
 
 console.log('Exercise 6 result:', foods);
 
 
-/*
-Exercise 7: Using the `slice()` method
+// /*
+// Exercise 7: Using the `slice()` method
 
-1) Use the `slice()` method to create a new array that contains 'sushi' and 
-   'cupcake'.
+// 1) Use the `slice()` method to create a new array that contains 'sushi' and 
+//    'cupcake'.
 
-2) Assign it to a variable named `yummy`.
+// 2) Assign it to a variable named `yummy`.
 
-Complete Exercise 7 in the space below:
-*/
+// Complete Exercise 7 in the space below:
+// */
 
 
 
 console.log('Exercise 7 result:', yummy);
 
 
-/*
-Exercise 8: Finding an index
+// /*
+// Exercise 8: Finding an index
 
-1) Using the `indexOf()` method, find the index of the string 'tofu' in the 
-   `foods` array. 
+// 1) Using the `indexOf()` method, find the index of the string 'tofu' in the 
+//    `foods` array. 
 
-2) Assign it to a variable named `soyIdx`.
+// 2) Assign it to a variable named `soyIdx`.
 
-Complete Exercise 8 in the space below:
-*/
+// Complete Exercise 8 in the space below:
+// */
 
 
 
 console.log('Exercise 8 result:', soyIdx);
 
 
-/*
-Exercise 9: Joining elements
+// /*
+// Exercise 9: Joining elements
 
-1) Use the `join()` method to concatenate the strings in the `foods` array, 
-   separated by ' -> '. 
+// 1) Use the `join()` method to concatenate the strings in the `foods` array, 
+//    separated by ' -> '. 
 
-2) Assign the result to a variable called `allFoods`. 
+// 2) Assign the result to a variable called `allFoods`. 
 
-Note: The final result should log as:
-'taco -> sushi -> cupcake -> tofu -> cheeseburger'
+// Note: The final result should log as:
+// 'taco -> sushi -> cupcake -> tofu -> cheeseburger'
 
-Complete Exercise 9 in the space below:
-*/
+// Complete Exercise 9 in the space below:
+// */
 
 
 
 console.log('Exercise 9 result:', allFoods);
 
 
-/*
-Exercise 10: Check for an element
+// /*
+// Exercise 10: Check for an element
 
-1) Using the .includes() method, check if the `foods` array contains the string
-   'soup'.
+// 1) Using the .includes() method, check if the `foods` array contains the string
+//    'soup'.
 
-2) Assign the result to a variable called `hasSoup``.
+// 2) Assign the result to a variable called `hasSoup``.
 
-Complete Exercise 10 in the space below:
-*/
+// Complete Exercise 10 in the space below:
+// */
 
 
 
 console.log('Exercise 10 result:', hasSoup);
 
 
-/*
-Exercise 11: Odd numbers from an array
+// /*
+// Exercise 11: Odd numbers from an array
 
-1) Choose a method to iterate through the `nums` array.
+// 1) Choose a method to iterate through the `nums` array.
 
-2) Push each odd number to a new array named `odds`.
+// 2) Push each odd number to a new array named `odds`.
 
-Hint: Initialize the `odds` variable to an empty array before the iteration.
+// Hint: Initialize the `odds` variable to an empty array before the iteration.
 
-Complete Exercise 11 in the space below:
-*/
+// Complete Exercise 11 in the space below:
+// */
 
 const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90];
 
@@ -161,24 +161,24 @@ const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90];
 console.log('Exercise 11 result:', odds);
 
 
-/*
-Exercise 12: FizzBuzz with arrays
+// /*
+// Exercise 12: FizzBuzz with arrays
 
-1) Choose a method to iterate through the `nums` array. 
+// 1) Choose a method to iterate through the `nums` array. 
 
-2. As you loop, sort the numbers into new arrays based on the following rules:
+// 2. As you loop, sort the numbers into new arrays based on the following rules:
 
-   - Push any number evenly divisible by 3 to an array called `fizz`.
-   - Push any number evenly divisible by 5 to an array called `buzz`.
-   - Push any number that is evenly divisible by 3 and 5 to an array called
-     `fizzbuzz`.
+//    - Push any number evenly divisible by 3 to an array called `fizz`.
+//    - Push any number evenly divisible by 5 to an array called `buzz`.
+//    - Push any number that is evenly divisible by 3 and 5 to an array called
+//      `fizzbuzz`.
 
-   Note: A single number may meet more than one of the above rules. If it does,
-         it should be placed in multiple arrays. For example, the number `15`
-         will appear in the `fizz`, `buzz`, and `fizzbuzz` arrays.
+//    Note: A single number may meet more than one of the above rules. If it does,
+//          it should be placed in multiple arrays. For example, the number `15`
+//          will appear in the `fizz`, `buzz`, and `fizzbuzz` arrays.
 
-Complete Exercise 12 in the space below:
-*/
+// Complete Exercise 12 in the space below:
+// */
 
 
 
@@ -188,17 +188,17 @@ console.log('  buzz:', buzz);
 console.log('  fizzbuzz:', fizzbuzz);
 
 
-/*
-Exercise 13: Retrieve the Last Array
+// /*
+// Exercise 13: Retrieve the Last Array
 
-1) Assign the last nested array in the `numArrays` below to a variable named
-   `numList`. As you do this, also fulfill these goals:
+// 1) Assign the last nested array in the `numArrays` below to a variable named
+//    `numList`. As you do this, also fulfill these goals:
 
-   - Assume you don't know how many nested arrays `numArrays` contains.
-   - Do not alter the original `numArrays` array.
+//    - Assume you don't know how many nested arrays `numArrays` contains.
+//    - Do not alter the original `numArrays` array.
 
-Complete Exercise 13 in the space below:
-*/
+// Complete Exercise 13 in the space below:
+// */
 
 const numArrays = [
 	[100, 5, 23],
@@ -210,35 +210,68 @@ const numArrays = [
 console.log('Exercise 13 result:', numList);
 
 
-/*
-Exercise 14: Accessing within nested arrays
+// /*
+// Exercise 14: Accessing within nested arrays
 
-1) Retrieve the number `66` from the `numArrays` array. As part of this process
-   do not alter the original `numArrays` array.
+// 1) Retrieve the number `66` from the `numArrays` array. As part of this process
+//    do not alter the original `numArrays` array.
 
-2) Assign it to a variable called `num`.
+// 2) Assign it to a variable called `num`.
 
-Complete Exercise 14 in the space below:
-*/
+// Complete Exercise 14 in the space below:
+// */
 
 
 
 console.log('Exercise 14 result:', num);
 
 
-/*
-Exercise 15: Nested array sum
+// /*
+// Exercise 15: Nested array sum
 
-1) Use nested loops or `forEach()` methods to sum up all numbers within 
-   `numArrays` nested arrays.
+// 1) Use nested loops or `forEach()` methods to sum up all numbers within 
+//    `numArrays` nested arrays.
    
-2) Assign the sum to a variable called `total`.
+// 2) Assign the sum to a variable called `total`.
 
-Hint: Be sure to declare and initialize the total variable before the iterations.
+// Hint: Be sure to declare and initialize the total variable before the iterations.
 
-Complete Exercise 15 in the space below:
-*/
+// Complete Exercise 15 in the space below:
+// */
 
 
 
 console.log('Exercise 15 result:\n', total);
+
+// BELOW IS CHALLENGES FROM KYLE
+/*
+Create a function that returns the CSV representation of a two-dimensional numeric array.
+
+Example:
+
+input:
+   [[ 0, 1, 2, 3, 4 ],
+    [ 10,11,12,13,14 ],
+    [ 20,21,22,23,24 ],
+    [ 30,31,32,33,34 ]] 
+    
+output:
+     '0,1,2,3,4\n'
+    +'10,11,12,13,14\n'
+    +'20,21,22,23,24\n'
+    +'30,31,32,33,34'
+Array's length > 2.
+*/
+
+function toCSVText(array) {
+    let stringResult = ""
+        for (i=0; i<array.length; i++) {
+            stringResult+=(`${array[i].join(",")}\n`)
+        } 
+    return stringResult
+ }
+
+ console.log(toCSVText([[ 0, 1, 2, 3, 4 ],
+    [ 10,11,12,13,14 ],
+    [ 20,21,22,23,24 ],
+    [ 30,31,32,33,34 ]]))
