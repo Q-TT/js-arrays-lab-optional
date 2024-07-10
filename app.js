c1f2475f1cb0e28eb6695e4560b524f504053ba3
Astrid Ge <qiutongg@umich.edu>
2024-07-09 19:05:38 -0700

    complete task13

--- app.js
+++ app.js
@@ -200,13 +200,13 @@ console.log('  fizzbuzz:', fizzbuzz);
 
 // Complete Exercise 13 in the space below:
 // */
-
 const numArrays = [
 	[100, 5, 23],
 	[15, 21, 72, 9],
 	[45, 66],
 	[7, 81, 90]
 ];
+const numList = numArrays[numArrays.length-1]
 
 console.log('Exercise 13 result:', numList);
 
